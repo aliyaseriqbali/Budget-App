6737ca281381459182248407d7587de275bb38e5
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-13 09:25:48 +0200

    Reading the input data

--- starter/app.js
+++ starter/app.js
@@ -8,17 +8,37 @@ var budgetController = (function () {
 // UI controller
 var UIController = (function () {
 
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
 
-
-
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, // will either inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+        getDOMStrings: function () {
+            return DOMStrings;
+        }
+    };
 
 })();
 
 
 // Global app controller
 var controller = (function (budgetCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMStrings();
+
     var ctrlAddItem = function () {
         // 1. Get the input data
+        var input = UIController.getInput();
+
         // 2. Add the item to the budget controller
         // 3. Add the new item to the IU
         // 4. Calculate the budget
@@ -26,15 +46,14 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
-
         if (event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
         }
     });
+
 
 
 

1fed0f987d496189bf0bd088ab3a167db44c425b
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-13 10:06:11 +0200

    Creating a init function

--- starter/app.js
+++ starter/app.js
@@ -33,7 +33,18 @@ var UIController = (function () {
 
 // Global app controller
 var controller = (function (budgetCtrl, UICtrl) {
-    var DOM = UICtrl.getDOMStrings();
+
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
 
     var ctrlAddItem = function () {
         // 1. Get the input data
@@ -44,17 +55,15 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 4. Calculate the budget
         // 5. Display the budget on the US
 
-    }
+    };
 
-    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function (){
+            console.log('application has started');
+            setupEventListeners();
         }
-    });
-
-
-
+    };
 
 })(budgetController, UIController);
+
+controller.init();

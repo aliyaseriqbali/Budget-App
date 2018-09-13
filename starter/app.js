96f00f0b98e3861a55b1340db8dc56e29a8689b5
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-13 13:00:43 +0200

    Adding new item to the budget controller

--- starter/app.js
+++ starter/app.js
@@ -21,9 +21,40 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+          // Create new ID
+          if (data.allItems[type].length > 0) {
+            ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+        } else {
+            ID = 0;
+        }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
         },
-    }
+        testing: function () {
+            console.log(data);
+        }
+
+    };
 })();
+
 
 // UI controller
 var UIController = (function () {
@@ -33,7 +64,7 @@ var UIController = (function () {
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputButton: '.add__btn'
-    }
+    };
 
     return {
         getInput: function () {
@@ -67,10 +98,13 @@ var controller = (function (budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItem = function () {
+        var input, newItem;
+
         // 1. Get the input data
-        var input = UIController.getInput();
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the new item to the IU
         // 4. Calculate the budget
         // 5. Display the budget on the US

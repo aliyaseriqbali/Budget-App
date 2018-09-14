d2f707f4997d8067765204f60e24f8e3c0fbcd62
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-14 13:30:07 +0200

    Finishing touches to improve UX

--- starter/app.js
+++ starter/app.js
@@ -169,6 +169,12 @@ var UIController = (function () {
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
     };
 
+    var nodeListForEach = function (list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i)
+        }
+    };
+
     return {
         getInput: function () {
             return {
@@ -231,11 +237,6 @@ var UIController = (function () {
 
         displayPercentages: function (percentages) {
             var fields = document.querySelectorAll(DOMStrings.expensesPercLabel);
-            var nodeListForEach = function (list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i)
-                }
-            };
 
             nodeListForEach(fields, function (current, index) {
                 if (percentages[index] > 0) {
@@ -255,6 +256,21 @@ var UIController = (function () {
 
             year = now.getFullYear();
             document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
+        changedType: function () {
+
+            var fields = document.querySelectorAll(
+                DOMStrings.inputType + ',' +
+                DOMStrings.inputDescription + ',' +
+                DOMStrings.inputValue
+            );
+
+            nodeListForEach(fields, function (cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMStrings.inputButton).classList.toggle('red');
 
         },
 
@@ -279,7 +295,9 @@ var controller = (function (budgetCtrl, UICtrl) {
             }
         });
 
-        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType)
 
     };
 
